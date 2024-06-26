b478a37d3b60e6ec8fb3b1bc64405a3bfa10aab5
jaylee <jayginwoolee98@gmail.com>
2024-06-26 20:05:33 +0900

    firebase initial set up

---
+++ src/Firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCiKTFz-L1f5dVpDwMoI2Q_wrprOPCou7w",
+    authDomain: "login-app-f49fc.firebaseapp.com",
+    projectId: "login-app-f49fc",
+    storageBucket: "login-app-f49fc.appspot.com",
+    messagingSenderId: "403928738187",
+    appId: "1:403928738187:web:a2c512c7f77730c639278e",
+    measurementId: "G-VW2899QKX3"
+  };
