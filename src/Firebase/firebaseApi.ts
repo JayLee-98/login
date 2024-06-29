e0144c4c503023724360f5b43131a49b3ac2f073
jaylee <jayginwoolee98@gmail.com>
2024-06-29 14:53:53 +0900

    Implement Image related apis to firebase api

--- src/Firebase/firebaseApi.ts
+++ src/Firebase/firebaseApi.ts
@@ -19,6 +19,7 @@ import {
   getFirestore,
   setDoc,
 } from "firebase/firestore";
+import {FirebaseStorage, getDownloadURL, getStorage, ref, uploadBytes} from "firebase/storage";
 import { UserInfo } from "../types";
 
 export default class FirebaseApi {
@@ -27,6 +28,7 @@ export default class FirebaseApi {
   auth: Auth;
   googleAuthProvider: GoogleAuthProvider;
   firestore: Firestore;
+  storage: FirebaseStorage;
 
   constructor() {
     this.app = initializeApp(firebaseConfig);
@@ -34,6 +36,7 @@ export default class FirebaseApi {
     this.auth = getAuth(this.app);
     this.googleAuthProvider = new GoogleAuthProvider();
     this.firestore = getFirestore(this.app);
+    this.storage = getStorage(this.app);
   }
 
   onAuthStateChanged = (nextOrObserver: NextOrObserver<User>): Unsubscribe => {
@@ -71,5 +74,17 @@ export default class FirebaseApi {
       username: docSnap.data().username,
       profilePicHandle: null,
     };
+  };
+
+  asyncUploadImage = async (userId: string, file: File): Promise<string> => {
+    const path = userId + '/' + file.name;
+    const storageRef = ref(this.storage, path);
+    const ret = await uploadBytes(storageRef, file);
+    return ret.ref.fullPath;
+  };
+
+  asyncGetURLFromHandle = async (handle: string): Promise<string> => {
+    const url = await getDownloadURL(ref(this.storage, handle));
+    return url;
   };
 };
